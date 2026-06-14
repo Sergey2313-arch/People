a3c528f32238ca5518d7e8cd055204d372cb065d
Sergey2313-arch <skorobkov435@gmail.com>
2026-06-14 16:54:56 +0300

    Add App component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Header';
+import People from './People';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <People />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
