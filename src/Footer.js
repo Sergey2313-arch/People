af865dd82c4fd335cad55556724e6fe417d9d3d9
Sergey2313-arch <skorobkov435@gmail.com>
2026-06-14 16:55:11 +0300

    Add Footer component

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: '#282C34', color: 'white', padding: '16px', fontSize: '18px' }}>
+      <p>People &copy; {new Date().getFullYear()}</p>
+    </footer>
+  );
+}
+
+export default Footer;
