0dff011216a9ee6282f2175a8a63761b5232e7e8
Sergey2313-arch <skorobkov435@gmail.com>
2026-06-14 17:08:57 +0300

    Redesign header component

--- src/Header.js
+++ src/Header.js
@@ -3,9 +3,19 @@ import './Header.css';
 function Header() {
   return (
     <header className="header">
-      <p className="header-label">Домашнее задание по React</p>
-      <h1>People</h1>
-      <p>Карточки людей, выложенные плитками</p>
+      <div className="header-content">
+        <p className="header-badge">React homework</p>
+        <h1>People Cards</h1>
+        <p className="header-subtitle">
+          Небольшое приложение с карточками людей, выложенными плиткой
+        </p>
+
+        <div className="header-tags">
+          <span>6 карточек</span>
+          <span>CSS Grid</span>
+          <span>React components</span>
+        </div>
+      </div>
     </header>
   );
 }
