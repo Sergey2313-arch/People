8280470bd190ed0f6626d4e58e759daa8fce84e4
Sergey2313-arch <skorobkov435@gmail.com>
2026-06-14 17:11:18 +0300

    Restore classic header template

--- src/Header.js
+++ src/Header.js
@@ -3,18 +3,8 @@ import './Header.css';
 function Header() {
   return (
     <header className="header">
-      <div className="header-content">
-        <h1>People Cards</h1>
-        <p className="header-subtitle">
-          Карточки людей, выложенные плитками
-        </p>
-
-        <div className="header-tags">
-          <span>6 карточек</span>
-          <span>CSS Grid</span>
-          <span>React components</span>
-        </div>
-      </div>
+      <h1>People</h1>
+      <p>Карточки людей, выложенные плитками</p>
     </header>
   );
 }
