905f5c681993b3be0539ca4b4a888b64243474b4
Sergey2313-arch <skorobkov435@gmail.com>
2026-06-14 17:13:18 +0300

    Make header like React template

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,13 @@ import './Header.css';
 function Header() {
   return (
     <header className="header">
+      <div className="people-logo" aria-hidden="true">👥</div>
       <h1>People</h1>
-      <p>Карточки людей, выложенные плитками</p>
+      <p>The cards of people in a tile layout</p>
+      <div className="header-buttons">
+        <a href="#cards" className="header-button primary">People</a>
+        <a href="https://github.com/Sergey2313-arch/People" className="header-button secondary">GitHub</a>
+      </div>
     </header>
   );
 }
