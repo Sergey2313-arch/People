3124ae96f8c235e40b698a5cc0150a76ff42fbe2
Sergey2313-arch <skorobkov435@gmail.com>
2026-06-14 16:55:03 +0300

    Add Header component

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <p className="header-label">Домашнее задание по React</p>
+      <h1>People</h1>
+      <p>Карточки людей, выложенные плитками</p>
+    </header>
+  );
+}
+
+export default Header;
